12f4f3a8fd97a1a9ccd45f82182402838458c3ee
scinny91 <bellaaascinny@gmail.com>
2020-01-19 15:21:35 +0100

    passato a fontawesome remoto

--- backand/start.py
+++ backand/start.py
@@ -10,23 +10,17 @@ from PIL import Image
 
 def start(request):
     url = request.META['PATH_INFO']
-    html = ''
+
     if url == '/html/':
         url += 'index.html'
 
-    with codecs.open(settings.STATIC_HTML + url, 'r', encoding='utf-8', errors='ignore') as content_file:
-        try:
-            html = content_file.read()
-        except:
-            print('file in errore: ' + settings.STATIC_HTML + url)
-            print(traceback.format_exc())
-
     if '.js' in url:
+        html = read_file(url)
         return HttpResponse(html, content_type="application/x-javascript")
     elif '.css' in url:
+        html = read_file(url)
         return HttpResponse(html, content_type="text/css")
     elif '.png' in url:
-        print('ciaooooo')
         img = Image.open(settings.STATIC_HTML + url)
         response = HttpResponse(content_type="image/png")
         img.save(response, "PNG")
@@ -43,7 +37,7 @@ def start(request):
         return response
     elif '.html' in url:
         # per l'html ci schiaffo dentro le costanti...
-
+        html = read_file(url)
         cookies = request.COOKIES
         if 'index.html' in url:
             html = html.format(**view.mostra_utenti_salvati(cookies))
@@ -53,13 +47,26 @@ def start(request):
         return ret
     elif '.ttf' in url:
         print(url)
+        html = read_file(url)
         return HttpResponse(html)
     elif '.woff' in url:
         print(url)
+        html = read_file(url)
         return HttpResponse(html, content_type='application/x-font-woff')
     else:
-        print(url)
+        html = read_file(url)
         return HttpResponse(html)
+
+def read_file(url):
+    html = ''
+    with codecs.open(settings.STATIC_HTML + url, 'r', encoding='utf-8', errors='strict') as content_file:
+        try:
+            html = content_file.read()
+        except:
+            print('file in errore: ' + settings.STATIC_HTML + url)
+            print(traceback.format_exc())
+            raise
+    return html
 
 
 def get_statics_file(request):
