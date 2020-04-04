722fdfb06bb899564874aeace72d245baee93c06
scinny91 <bellaaascinny@gmail.com>
2020-04-04 17:31:01 +0200

    Nuovo FE profilazione utenti

--- backand/costants.py
+++ backand/costants.py
@@ -78,39 +78,44 @@ def_usr_data = {
 
 
 blocco_righe_invitato = """
-    <div class="row" id="riga_invitato_{id_ospite}">
+    <div class="row riga_invitato" id="riga_invitato_{id_ospite}">
 
             <div class="col-sm-3">
-                    <div class='delete_ospite'  id='cancella_ospite_{id_ospite}'>
-                        <i class="fas fa-user-times"></i>
-                    </div>
                     <img src="../{url_img_user}" alt="Circle Image" class="img-circle" id='img_{id_ospite}'>
-                </div>
+                    
+            </div>
             <div class="col-md-9">
                 <div class="row">
+                    <div class="col-sm-3 lbl">
+                        <input type="file" class="form-control-file" id="foto_{id_ospite}" onchange='uploadFile(this)' name="foto_{id_ospite}" style="display: none;" >
+                        <label for="foto_{id_ospite}" >
+                            <span> <i class="fas fa-upload fa-2x"></i> Carica la foto!</span>
+                        </label>
+                    </div>
+                    <div class="col-sm-3 lbl">
+                       <div class='delete_ospite'  id='cancella_ospite_{id_ospite}'>
+                           <label>
+                                <span> <i class="fas fa-user-times fa-2x"></i>  Elimina ospite!</span>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
                     <div class="col-sm-4 lbl">
-                        <div class="form-group">
-                             <label class="control-label">Nome:</label>
+                        <div class="input-group">
+                            <span class="input-group-addon">Nome:</span>
                             <input type="text" value="{nome}" name='nome' placeholder="Digita il nome dell'ospite" class="form-control onchangeupdate" id='nome_ospite_{id_ospite}' />
                         </div>
                     </div>
-                    <div class="col-sm-1 lbl">
-                        <div class="form-group {mostra_albergo}">
-                            <label class="control-label" >Albergo:</label>
-                            <div class="switch-unload" id='switchAlbergo_{id_ospite}'  data-on-label="<i class='fa fa-check'></i>" data-off-label="<i class='fa fa-times'></i>">
-                                  <input type="checkbox" {albergo} id='albergo_{id_ospite}' class='onchangeupdate'  name='albergo'/>
-                            </div>
-                        </div>
-                    </div>
                     <div class="col-sm-4 lbl">
-                        <div class="form-group">
-                            <label class="control-label">Mail:</label>
+                        <div class="input-group">
+                            <span class="input-group-addon">Mail:</span>
                             <input type="text" value="{mail}" name='mail' placeholder="Inserisci un indirizzo mail" class="form-control onchangeupdate" id='mail_{id_ospite}' />
                         </div>
                     </div>
-                    <div class="col-sm-3 lbl">
-                        <div class="form-group">
-                            <label class="control-label">Sesso:</label>
+                    <div class="col-sm-4 lbl">
+                        <div class="input-group">
+                            <span class="input-group-addon">Sesso:</span>
                             <select class="form-control onchangeupdate" id="sesso_{id_ospite}" name='sesso'>
                                   <option {select_uomo} value='Uomo'>Uomo</option>
                                   <option {select_donna} value='Donna'>Donna</option>
@@ -121,8 +126,14 @@ blocco_righe_invitato = """
                 </div>
                 <div class="row">
                     <div class="col-sm-4 lbl">
-                        <div class="form-group">
-                            <label class="control-label">Menu:</label>
+                        <div class="input-group">
+                            <span class="input-group-addon">Note:</span>
+                             <textarea class="form-control onchangeupdate" id="note_{id_ospite}" rows="2" name='note'></textarea>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 lbl">
+                        <div class="input-group">
+                            <span class="input-group-addon">Menu:</span>
                             <select class="form-control onchangeupdate" id="menu_{id_ospite}" name='menu'>
                                   <option {select_bambino} value='bambino'>Bambino</option>
                                   <option {select_adulto} value='adulto'>Carne</option>
@@ -131,14 +142,24 @@ blocco_righe_invitato = """
                             </select>
                         </div>
                     </div>
+                    
                     <div class="col-sm-4 lbl">
-                          <label class="control-label">Note:</label>
-                         <textarea class="form-control onchangeupdate" id="note_{id_ospite}" rows="2" name='note'></textarea>
+                        <div class="input-group {mostra_albergo}">
+                           <span class="input-group-addon">Albergo:</span>
+                            <div class="input-group-addon">
+                                <div class="switch-unload" id='switchAlbergo_{id_ospite}'  
+                                data-on-label="<i class='fa fa-check'></i>" 
+                                data-off-label="<i class='fa fa-times'></i>">
+                                  <input type="checkbox" {albergo} id='albergo_{id_ospite}' class='onchangeupdate'  name='albergo'/>
+                            </div>
+                             </div>
+                           
+                        </div>
                     </div>
-                    <div class="col-sm-4 lbl">
-                        <label class="control-label">Foto:</label>
-                        <input type="file" class="form-control-file" id="foto_{id_ospite}" onchange='uploadFile(this)'>
-                    </div>
+                    
+                    
+                    
+                    
                 </div>
             </div>
     </div>
@@ -146,3 +167,4 @@ blocco_righe_invitato = """
 
 def get_costants():
     return globals()
+
