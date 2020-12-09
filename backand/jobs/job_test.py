218d974d0118361e5aa7111ea13f2e88d14a0371
scinny91 <bellaaascinny@gmail.com>
2020-12-09 22:12:06 +0100

    loggata job test

--- backand/jobs/job_test.py
+++ backand/jobs/job_test.py
@@ -1,9 +1,8 @@
 
 from matrimonio.backand.bo import base
-
+from datetime import datetime
 def main(params):
     print('ciao, io devo inviare la mail')
 
-
-    for famiglia in base.Famiglia.objects.filter():
-        print(famiglia.__dict__)
+    with open("file.txt", "w") as myFile:
+        myFile.write(str(datetime.now()))
